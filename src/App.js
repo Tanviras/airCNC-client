8d0a7ff2fd85d7cbcdc3d29a6137e96b589262f0
Tanviras <tanvirasme@gmail.com>
2021-05-01 20:24:05 +0600

    experiences sent to backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Experiences from "./Components/Experiences/Experiences";
+import Home from "./Components/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Switch>
+
+          <Route exact path='/'>
+          <Home></Home>
+          </Route>
+
+          <Route path='/addExperiences'>
+          <Experiences></Experiences>
+          </Route>
+
+
+        </Switch>
+      </Router>
+    
     </div>
   );
 }
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            Home
+            <br/>
+
+            <a href='/addExperiences'>
+            <button className='btn btn-primary'>Add Experiences</button>
+            </a>
+           
+        </div>
+    );
+};
+
+export default Home;
